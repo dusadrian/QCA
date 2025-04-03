730b2d5bbd66672808b666eef62138485dde83cc
Adrian Dușa <dusa.adrian@gmail.com>
2025-04-03 12:05:52 -0500

    up minor version

--- inst/gui/www/js/maincode.js
+++ inst/gui/www/js/maincode.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2016 - 2024, Adrian Dusa
+Copyright (c) 2016 - 2025, Adrian Dusa
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -7087,7 +7087,7 @@ $("#menu_about").click(function() {
     else {
         createDialog(settings["about"]);
         var messages = [
-            "R package: QCA, version 3.22.5",
+            "R package: QCA, version 3.23.3",
             "",
             "Author: Adrian Dușa (dusa.adrian@unibuc.ro)",
             "Former coauthors:",
